c7424b967234d9a8889a717eee979f27adca241b
nelson.ntepp <nelson.ntepp@guarani.fr>
2022-08-01 11:36:40 +0200

    init-swagger

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma.service';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
+
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Podologue')
+    .setDescription('The podologue API description')
+    .setVersion('1.0')
+    .addTag('pdlg')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/swagger', app, document);
 
   await app.listen(8080);
 }
