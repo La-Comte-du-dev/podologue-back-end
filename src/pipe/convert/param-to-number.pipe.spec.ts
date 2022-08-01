d26e5fa11393128ede060bb04c49c04a9d31bee0
Nelson Ntepp <73397655+Neruso973@users.noreply.github.com>
2022-08-01 17:24:33 +0200

    Joi (#1)

---
+++ src/pipe/convert/param-to-number.pipe.spec.ts
@@ -0,0 +1,7 @@
+import { ParamToNumberPipe } from './param-to-number.pipe';
+
+describe('ParamToNumberPipe', () => {
+  it('should be defined', () => {
+    expect(new ParamToNumberPipe()).toBeDefined();
+  });
+});
---
+++ src/schemas/joi-auth-schema.ts
@@ -0,0 +1,29 @@
+import Joi from 'joi';
+import { joiPassword as JoiPassword } from 'joi-password';
+
+export const signupSchema = Joi.object({
+  username: Joi.string().min(3).max(20).required(),
+  email: Joi.string().email().max(255).required(),
+  password: JoiPassword.string()
+    .min(8)
+    .max(20)
+    .minOfSpecialCharacters(1)
+    .minOfLowercase(1)
+    .minOfUppercase(1)
+    .minOfNumeric(1)
+    .noWhiteSpaces()
+    .required(),
+});
+
+export const signinSchema = Joi.object({
+  username: Joi.string().min(3).max(20).required(),
+  password: JoiPassword.string()
+    .min(8)
+    .max(20)
+    .minOfSpecialCharacters(1)
+    .minOfLowercase(1)
+    .minOfUppercase(1)
+    .minOfNumeric(1)
+    .noWhiteSpaces()
+    .required(),
+});
