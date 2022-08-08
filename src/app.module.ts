9c26d29739886b27fe569ea4125d7d82b8695d9d
Axel Cabanat <axelcabanat@gmail.com>
2022-08-08 18:52:47 +0200

    patient init

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
+import { PatientsModule } from './routes/patients/patients.module';
 
 @Module({
-  imports: [],
+  imports: [PatientsModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
