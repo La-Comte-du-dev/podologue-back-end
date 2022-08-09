fb3d1cbb422ecbc6806f9b0fd2f4487898e1d578
Axel Cabanat <axelcabanat@gmail.com>
2022-08-09 16:40:02 +0200

    add prismaservice to patientmodule

--- src/routes/patients/patients.module.ts
+++ src/routes/patients/patients.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 import { PatientsController } from './patients.controller';
 import { PatientsService } from './patients.service';
 
 @Module({
   controllers: [PatientsController],
-  providers: [PatientsService],
+  providers: [PatientsService, PrismaService],
 })
 export class PatientsModule {}
